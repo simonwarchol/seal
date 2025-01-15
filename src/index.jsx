340d990521a3da3059221ec3c17d05ea73f94db6
Simon Warchol <simonwarchol@g.harvard.edu>
2025-01-15 13:16:36 -0500

    Working both hosted and not

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import Seal from './seal/seal.jsx';
 
+function App() {
+  const [value, setValue] = useState(0);
+  return (
+    <React.StrictMode>
+      <Seal value={value} setValue={setValue} />
+    </React.StrictMode>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Seal />
-  </React.StrictMode>,
-);
+root.render(<App />);
