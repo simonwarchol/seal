730b45acc54f643829852c981df4c5a0373aa655
Simon Warchol <simonwarchol@g.harvard.edu>
2025-01-15 20:29:30 -0500

    Removing two

---
+++ rsbuild.config.mjs
@@ -0,0 +1,59 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginBabel } from '@rsbuild/plugin-babel';
+
+const ReactCompilerConfig = {
+  target: '18',
+};
+export default defineConfig({
+  environments: {
+    widget: {
+      output: {
+        distPath: {
+          root: 'widget/',
+        },
+        filename: {
+          js: 'seal.js',
+          css: 'seal.css',
+        },
+      },
+
+      performance: {
+        chunkSplit: {
+          strategy: 'all-in-one',
+        },
+      },
+      tools: {
+        htmlPlugin: false,
+        rspack: (config) => {
+          config.experiments.outputModule = true;
+          config.output.library = {
+            type: 'module',
+          };
+          return config;
+        },
+      },
+      source: {
+        entry: {
+          index: './src/widget.jsx',
+        },
+      },
+    },
+    web: {
+      output: {
+        distPath: {
+          root: 'build/dist/',
+        },
+      },
+    },
+  },
+  plugins: [pluginReact(), pluginBabel({
+    include: /\.(?:jsx|tsx)$/,
+    babelLoaderOptions(opts) {
+      opts.plugins?.unshift([
+        'babel-plugin-react-compiler',
+        ReactCompilerConfig,
+      ]);
+    },
+  })],
+});
